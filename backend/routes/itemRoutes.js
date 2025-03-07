eaa750d1f1f935f16e474d1ca9d9a850c7a4add6
RicardoMaga <ricardontmagalhaes04@gmail.com>
2025-03-07 14:28:12 +0000

    backend:Routes_ler_item

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -6,9 +6,17 @@ const router = express.Router();
 
 //Criar Item
 
-router.post ("/", async (req, res)=>{
+router.post("/", async (req, res) => {
     const newItem = new Item(req.body);
     await newItem.save();
     res.json(newItem);
 });
+
+//Ler Item
+
+router.get("/", async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
 
